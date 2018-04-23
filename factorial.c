7310a7207c5ce144c5e942da446027b71fd0b073
ETimakova <35500037+ETimakova@users.noreply.github.com>
2018-04-23 15:19:06 -0700

    Add files via upload
    Trying to validate user input as an integer using while loop and scanf

--- factorial.c
+++ factorial.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
 #include <math.h>
 
+
+//pick long long unsigned int because it will allow the greatest value
 long long unsigned int factorial(long long unsigned int n);
 
-long long unsigned int main(){
+int main(){
 	long long unsigned int num;
+	//ask user for input (integer to find factorial of)
 	printf("This is a handy dandy factorial calculator. A factorial is defined as an integer multiplied by every integer that comes before it." "\nPlease enter an integer you would like to calculate the factorial of:");
-	scanf("%llu", &num);
-	printf("factorial is: %llu", factorial(num));
+	//do not use scanf because it assumes that you will get correctly formated input.. so good if its from another program, but not user input
+	//while loop to check validity of user input (checks for positive whole number)
+	while (scanf("%llu",&num)<0)
+		{
+  			printf("Invalid input. Please enter an integer: ");
+  			//asterisk tells scanf to read and ignore the value
+  			scanf("%*s");
+		}
+
+	//scanf("%llu", &num);
+	//prints answer to factorial equation based on user input
+	printf("The factorial is: %llu \n", factorial(num));
 	return 0;
 
 }
 
+//function finds factorial based on user input
 long long unsigned int factorial(long long unsigned int n){
 	if (n > 1)
 		return n * factorial(n-1);
-	else if (n < 1)
-		printf("Error, you must input a positive integer.");
+	//don't need part commented out below because already validated user input earlier in the code
+	/*else if (n < 1 )
+		printf("Error, you must input a positive integer.");*/
+	//else must return n because want to stop the loop when n=1 otherwise it is no long a factorial, so must return n=1 to solve
 	else 
 		return n;
 }	
+
+//if you change everything back to int instead of long long unsigned int, it performs in else if, but now it doesn't 
